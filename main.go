e1108efe314dfe197399e8e582cb5e7e68edc2f9
hokita <okita@enigmo.co.jp>
2020-12-27 18:13:26 +0900

    Add toppage and post weight action.

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"html/template"
+	"io/ioutil"
 	"net/http"
 	"os"
 
@@ -21,13 +22,55 @@ func run() error {
 	fmt.Println("start server")
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
-	})
+	r.HandleFunc("/", handle).Methods("GET")
+	r.HandleFunc("/weights/", postHandle).Methods("POST")
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	tpl := template.Must(template.ParseFiles("templates/index.html"))
+
+	b, err := ioutil.ReadFile("data.txt")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	m := map[string]string{
+		"Result": string(b),
+	}
+	tpl.Execute(w, m)
+}
+
+func postHandle(w http.ResponseWriter, r *http.Request) {
+	tpl := template.Must(template.ParseFiles("templates/index.html"))
+
+	f, err := os.OpenFile("data.txt", os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	}()
+
+	fmt.Fprintln(f, r.FormValue("weight"))
+
+	b, err := ioutil.ReadFile("data.txt")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	m := map[string]string{
+		"Result": string(b),
+	}
+	tpl.Execute(w, m)
 }
